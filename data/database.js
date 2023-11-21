5a76c1ba9f2fb3fef1837ba2c77181acecd7d8da
TheSoumyaDas <das8soumya@gmail.com>
2023-11-22 04:03:45 +0530

    Fixed some issues in database and package.json

--- data/database.js
+++ data/database.js
@@ -3,9 +3,10 @@ import mongoose from "mongoose";
 // DB Conn
 export const connectDB = () => {
   mongoose
-    .connect(process.env.MONGO_URI, {
-      dbName: "backendApi",
-    })
-    .then(() => console.log("DB connected"))
+    // .connect(process.env.MONGO_URI, {
+    //   dbName: "backendApi",
+    // })
+    .connect(process.env.MONGO_URI)
+    .then((c) => console.log(`Database Connected with ${c.connection.host}`))
     .catch((e) => console.log(e));
 };
